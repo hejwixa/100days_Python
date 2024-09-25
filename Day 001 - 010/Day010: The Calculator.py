4d80010f26b7b66ac03a82e9576d9400f839ca08
Wiktoria <ww.goralczyk@gmail.com>
2024-09-25 10:32:11 +0200

    Create Day010: The Calculator.py

---
+++ Day 001 - 010/Day010: The Calculator.py
@@ -0,0 +1,63 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+def calculator():
+    print(logo)
+    should_work = True
+    number_1 = float(input("What is the first number?\n"))
+
+    while should_work:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation\n")
+        number_2 = float(input("What is the second number?\n"))
+        answer = operations[operation_symbol](number_1, number_2)
+        print(f"{number_1} {operation_symbol} {number_2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation:\n").lower()
+
+        if choice == "y":
+            number_1 = answer
+        else:
+            should_work = False
+            print("\n" * 20)
+            calculator()
+
+calculator()
